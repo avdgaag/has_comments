6041764279fcafa9df6e6f613aaf6a398d500b22
Arjan van der Gaag <info@agwebdesign.nl>
2009-04-28 08:10:03 +0200

    Fix generator to use fixed names

--- generators/has_comments/has_comments_generator.rb
+++ generators/has_comments/has_comments_generator.rb
@@ -1,11 +1,8 @@
-class HasCommentsGenerator < Rails::Generator::NamedBase
+class HasCommentsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file 'akismet.yml', 'config/akismet.yml'
-
-      unless options[:skip_migration]
-        m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'create_comments'
-      end
+      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'create_comments'
     end
   end
 end
